fa6cf30a69c039993ba87e2525cdefbd1ee6cd1f
e.unurbayar83@gmail.com <e.unurbayar83@gamil.com>
2021-10-06 18:06:39 -0500

    added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,30 @@
+/*
+import React, and React Hooks:
+useState, useHistory, useEffect, 
+
+import Links from react router dom
+
+State: variables that update as the user makes changes
+an array called attractions
+Props: variables that are immutable (they don't change);
+
+
+when Home mounts, we'll run our useEffect to:
+    FETCH our attractions from the api!
+    STORE the attractions in a an array.
+
+Our useEffect won't need to update.
+
+functions:
+   onClick function that handles the logic for any buttons being clicked.
+
+we're going to RETURN:
+list of attractions
+maping thru the attractions array, and showing each attraction 
+we want to see each attractions detail by clicking on a attraction - we'll use Links to do it.
+
+we want to be able to add/delete attractions by clicking on a button, or perhaps a Link. 
+    
+STRETCH -- 
+we want to be able to search for attractions from the attractions array by different properties
+*/
